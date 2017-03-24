b77a3e5dd7759e7b7b965132852a33381d4a7c38
Houzl <zhilonghou@gmail.com>
2017-03-24 11:31:42 -0400

    regenerate

--- vantivsdk/fields.py
+++ vantivsdk/fields.py
@@ -1,7 +1,7 @@
 # ./fields.py
 # -*- coding: utf-8 -*-
 # PyXB bindings for NM:f03bd2be92cca0df00d3d054794f8a47756ec009
-# Generated 2017-03-22 14:36:32.116573 by PyXB version 1.2.5 using Python 3.4.6.final.0
+# Generated 2017-03-24 11:29:55.583521 by PyXB version 1.2.5 using Python 2.7.13.final.0
 # Namespace http://www.litle.com/schema
 
 from __future__ import unicode_literals
@@ -14,7 +14,7 @@ import pyxb.utils.domutils
 import sys
 import pyxb.utils.six as _six
 # Unique identifier for bindings created at the same time
-_GenerationUID = pyxb.utils.utility.UniqueIdentifier('urn:uuid:77947030-0f2e-11e7-895b-001a4a01063e')
+_GenerationUID = pyxb.utils.utility.UniqueIdentifier('urn:uuid:ba742624-10a6-11e7-8232-001a4a01063e')
 
 # Version of PyXB used to generate the bindings
 _PyXBVersion = '1.2.5'
