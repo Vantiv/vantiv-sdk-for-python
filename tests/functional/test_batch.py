e463ee3fefd3c03f2a675f12631f0f7599783ef0
Thien Dinh - Software Developer <tdinhcs@gmail.com>
2018-03-08 17:18:23 -0500

    Fixed commented code for testing.

--- tests/functional/test_batch.py
+++ tests/functional/test_batch.py
@@ -150,12 +150,6 @@ class TestBatch(unittest.TestCase):
                 self.fail("Timeout for retrieve batch response")
                 break        
 
-        #local_path = os.path.join(conf.batch_requests_path, '%s.xml' % withEncryptionFilename)
-        # encryptionReader = open(withEncryptionReponseFilepath, 'r')
-        # temp = open('localCopy', 'w')
-        # temp.write(encryptionReader.read())
-        # encryptionReader.close()
-        # temp.close()   
         call(["cat", withEncryptionReponseFilepath])
         # Undo setting configuration.
         conf.user = conf.txn_user
@@ -169,95 +163,95 @@ class TestBatch(unittest.TestCase):
             obj = fields.CreateFromDocument(xml_file.read())
             self.assertEquals("Valid Format", obj.message)
 
-    # def test_batch_submit(self):
-    #     # Initial Transactions container
-    #     transactions = batch.Transactions()
-    #     # Transactions presented by dict
-    #     txn_dict = {
-    #         'authorization':{
-    #             'reportGroup': 'Planets',
-    #             'orderId': '12344',
-    #             'amount': '106',
-    #             'orderSource': 'ecommerce',
-    #             'id': 'thisisid',
-    #             'card': {
-    #                 'expDate': '1210',
-    #                 'number': '4100000000000000',
-    #                 'type': 'VI',
-    #             }
-    #         }
-    #     }
-    #     transactions.add(txn_dict)
+    def test_batch_submit(self):
+        # Initial Transactions container
+        transactions = batch.Transactions()
+        # Transactions presented by dict
+        txn_dict = {
+            'authorization':{
+                'reportGroup': 'Planets',
+                'orderId': '12344',
+                'amount': '106',
+                'orderSource': 'ecommerce',
+                'id': 'thisisid',
+                'card': {
+                    'expDate': '1210',
+                    'number': '4100000000000000',
+                    'type': 'VI',
+                }
+            }
+        }
+        transactions.add(txn_dict)
 
-    #     # Card
-    #     card = fields.cardType()
-    #     card.number = '4457010000000009'
-    #     card.expDate = '0121'
-    #     card.cardValidationNum = '349'
-    #     card.type = 'VI'
+        # Card
+        card = fields.cardType()
+        card.number = '4457010000000009'
+        card.expDate = '0121'
+        card.cardValidationNum = '349'
+        card.type = 'VI'
 
-    #     # eCheck
-    #     # echeck = fields.echeck()
-    #     # echeck.accType = 'Checking'
-    #     # echeck.accNum = '4099999992'
-    #     # echeck.routingNum = '011075150'
+        # eCheck
+        # echeck = fields.echeck()
+        # echeck.accType = 'Checking'
+        # echeck.accNum = '4099999992'
+        # echeck.routingNum = '011075150'
 
-    #     # billtoaddress
-    #     billtoaddress = fields.contact()
-    #     billtoaddress.firstName = 'Mike'
-    #     billtoaddress.middleInitial = 'J'
-    #     billtoaddress.lastName = 'Hammer'
-    #     billtoaddress.phone = '999-999-9999'
+        # billtoaddress
+        billtoaddress = fields.contact()
+        billtoaddress.firstName = 'Mike'
+        billtoaddress.middleInitial = 'J'
+        billtoaddress.lastName = 'Hammer'
+        billtoaddress.phone = '999-999-9999'
 
-    #     # Initial authorization
-    #     authorization = fields.authorization()
-    #     authorization.orderId = '1'
-    #     authorization.amount = 10010
-    #     authorization.reportGroup = 'Planets'
-    #     authorization.orderSource = 'ecommerce'
-    #     authorization.card = card
-    #     authorization.billtoaddress = billtoaddress
-    #     authorization.id = 'thisisid'
-    #     # Add transaction to container
-    #     transactions.add(authorization)
+        # Initial authorization
+        authorization = fields.authorization()
+        authorization.orderId = '1'
+        authorization.amount = 10010
+        authorization.reportGroup = 'Planets'
+        authorization.orderSource = 'ecommerce'
+        authorization.card = card
+        authorization.billtoaddress = billtoaddress
+        authorization.id = 'thisisid'
+        # Add transaction to container
+        transactions.add(authorization)
 
-    #     # Initial authorization
-    #     authorization2 = fields.authorization()
-    #     authorization2.orderId = '2'
-    #     authorization2.amount = 1001
-    #     authorization2.reportGroup = 'Planets'
-    #     authorization2.orderSource = 'ecommerce'
-    #     authorization2.card = card
-    #     authorization2.billtoaddress = billtoaddress
-    #     authorization2.id = 'thisisid'
-    #     # Add transaction to container
-    #     transactions.add(authorization2)
+        # Initial authorization
+        authorization2 = fields.authorization()
+        authorization2.orderId = '2'
+        authorization2.amount = 1001
+        authorization2.reportGroup = 'Planets'
+        authorization2.orderSource = 'ecommerce'
+        authorization2.card = card
+        authorization2.billtoaddress = billtoaddress
+        authorization2.id = 'thisisid'
+        # Add transaction to container
+        transactions.add(authorization2)
 
-    #     # Initial authorization
-    #     sale = fields.sale()
-    #     sale.orderId = '1'
-    #     sale.amount = 10010
-    #     sale.reportGroup = 'Planets'
-    #     sale.orderSource = 'ecommerce'
-    #     sale.card = card
-    #     sale.billtoaddress = billtoaddress
-    #     sale.id = 'thisisid'
-    #     # Add transaction to container
-    #     transactions.add(sale)
+        # Initial authorization
+        sale = fields.sale()
+        sale.orderId = '1'
+        sale.amount = 10010
+        sale.reportGroup = 'Planets'
+        sale.orderSource = 'ecommerce'
+        sale.card = card
+        sale.billtoaddress = billtoaddress
+        sale.id = 'thisisid'
+        # Add transaction to container
+        transactions.add(sale)
 
-    #     filename = 'batch_test_%s' % datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+        filename = 'batch_test_%s' % datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
 
-    #     # stream to Vaitiv eCommerce and get object as response
-    #     response = batch.submit(transactions, conf, filename)
+        # stream to Vaitiv eCommerce and get object as response
+        response = batch.submit(transactions, conf, filename)
 
-    #     with open(os.path.join(conf.batch_requests_path, '%s.xml' % filename), 'r') as xml_file:
-    #         obj = fields.CreateFromDocument(xml_file.read())
-    #         self.assertEquals(1, obj.numBatchRequests)
-    #         self.assertEquals(11117, obj.batchRequest[0].authAmount)
+        with open(os.path.join(conf.batch_requests_path, '%s.xml' % filename), 'r') as xml_file:
+            obj = fields.CreateFromDocument(xml_file.read())
+            self.assertEquals(1, obj.numBatchRequests)
+            self.assertEquals(11117, obj.batchRequest[0].authAmount)
 
-    #     self.assertEquals('%s.xml.asc' % filename, response)
+        self.assertEquals('%s.xml.asc' % filename, response)
 
-"""
+
     def test_batch_rfr(self):
         # Initial Transactions container
         transactions = batch.Transactions()
@@ -597,7 +591,6 @@ class TestBatch(unittest.TestCase):
                 self.fail("Timeout for retrieve rfr batch response")
                 break
 
-"""
 
 if __name__ == '__main__':
     unittest.main()
